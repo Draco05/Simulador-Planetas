af6d9f2890d4dc746284b7ef4f0d40516c4de022
Caio Draco <79700943+Draco05@users.noreply.github.com>
2024-12-02 21:54:13 -0300

    Função de pause, reset e adicionar plantea

--- projeto.py
+++ projeto.py
@@ -2,7 +2,7 @@
 
 # Bibliotecas importadas
 import pygame
-from math import sqrt
+from math import sqrt, cos, sin, radians
 
 # Inicializar o Pygame
 pygame.init()
@@ -25,7 +25,7 @@ AMARELO = pygame.Color(255, 255, 0)
 # Janela do programa
 COMPRIMENTO, ALTURA = 800, 800
 JANELA = pygame.display.set_mode((COMPRIMENTO, ALTURA))
-
+pygame.display.set_caption("Simulador do Problema dos 3 corpos")
 
 # Definição da classe Planeta
 class Planeta:
@@ -73,7 +73,7 @@ class Planeta:
         x = self.pos_x * escala + deslocamento[0]
         y = self.pos_y * escala + deslocamento[1]
 
-        raio = self.raio * escala * 1000 # fora da escala por motivos visuais
+        raio = self.raio * escala * 1000 #fora de escala por motivos visuais
         self.trajetoria.append((self.pos_x, self.pos_y))
         pygame.draw.circle(JANELA, self.cor, (x, y), raio)
         if len(self.trajetoria) >= 2:
@@ -81,6 +81,7 @@ class Planeta:
             for ponto in self.trajetoria:
                 pontos.append((ponto[0] * escala + deslocamento[0], ponto[1] * escala + deslocamento[1]))
             pygame.draw.lines(JANELA, self.cor, False, pontos)
+        
 
     def checar_colisoes(self, planetas):
         for planeta in planetas:
@@ -92,8 +93,51 @@ class Planeta:
                     return True
         return False
 
-def modificar_estado_teclas(teclas, event, escala, deslocamento):
-     
+def adicionar_planeta(planetas, escala, deslocamento):
+    x, y = pygame.mouse.get_pos()
+    x = (x - deslocamento[0]) / escala
+    y = (y - deslocamento[1]) / escala
+    
+    erro_entrada = True
+    while (erro_entrada):
+        erro_entrada = False
+        massa = float(input("Digite a massa dor planeta em kg: "))
+        if massa <= 0:
+            print("Massa deve ter um valor estritamente positivo")
+            erro_entrada = True
+            continue
+
+        print("Digite a cor do planeta em RGB: ")
+        cor = []
+        for i in ["vermelho", "verde", "azul"]:
+            valor = int(input(f"Valor da cor {i}: "))
+            if valor > 255 or valor < 0:
+                print("Valor RGB deve estar entre 0 e 255")
+                erro_entrada = True
+                break
+            cor.append(valor)
+        
+        if erro_entrada: continue
+
+        vel = float(input("Digite o módulo da velocidade em m/s: "))
+        if vel < 0:
+            vel = -vel
+        angulo = float(input("Digite o ângulo (em graus) do versor da velocidade em relação ao eixo x: "))
+        vel_x = +vel * cos(radians(angulo))
+        vel_y = -vel * sin(radians(angulo))
+
+        raio = float(input("Digite o tamanho do raio em metros: "))
+        if raio <= 0:
+            print("O raio deve ser estritamente positivo")
+            erro_entrada = True
+        
+    print("Planeta criado!")
+
+    return Planeta(massa, cor, (vel_x, vel_y), raio, (x, y))
+
+
+
+def evento_teclas(teclas, event, escala, deslocamento, pause, planetas, colisao):
     if event.type == pygame.KEYDOWN:  # Verifica se uma tecla foi pressionada
         if event.key == pygame.K_UP:
             teclas[0] = True
@@ -107,11 +151,22 @@ def modificar_estado_teclas(teclas, event, escala, deslocamento):
             deslocamento[0] += COMPRIMENTO * (-0.25 * escala) / (2 * ESCALA_INICIAL) 
             deslocamento[1] += COMPRIMENTO * (-0.25 * escala) / (2 * ESCALA_INICIAL)
             escala *= 1.25
-
         if event.key == pygame.K_o:
             deslocamento[0] += COMPRIMENTO * (.2 * escala) / (2 * ESCALA_INICIAL) 
             deslocamento[1] += COMPRIMENTO * (0.2 * escala) / (2 * ESCALA_INICIAL)
             escala *= 0.8
+        if event.key == pygame.K_SPACE:
+            pause = not pause
+        if event.key == pygame.K_r:
+            planetas.clear()
+            deslocamento[0] = 0
+            deslocamento[1] = 0
+            escala = ESCALA_INICIAL
+            pause = True
+            colisao = False
+        if event.key == pygame.K_a:
+            planetas.append(adicionar_planeta(planetas, escala, deslocamento))
+    
 
     elif event.type == pygame.KEYUP: # Verifica se uma tecla foi soltada
         if event.key == pygame.K_UP:
@@ -122,7 +177,7 @@ def modificar_estado_teclas(teclas, event, escala, deslocamento):
             teclas[2] = False
         if event.key == pygame.K_RIGHT:
             teclas[3] = False
-    return escala
+    return escala, pause, colisao
 
 def mover_tela(deslocamento, teclas):
     deslocamento[0] += 5*(teclas[2] - teclas[3])
@@ -132,30 +187,28 @@ def mover_tela(deslocamento, teclas):
 def main():
     rodando = True
     colisao = False
+    pause = True
     clock = pygame.time.Clock()
     deslocamento = [0, 0]
     teclas_seguradas = [False, False, False, False] # up, down, left, right
     escala = ESCALA_INICIAL
+    planetas = []
 
-    # testes
-    planetas = [Planeta(6e30, AZUL, (-12.8e3, -1e4), 6.4e7, (COMPRIMENTO / 2 / escala, (ALTURA / 2 + 150) / escala)),
-                Planeta(2e30, AMARELO, (30e3, 0), 7e7, (COMPRIMENTO / 2 / escala, ALTURA / 2 / escala)),
-                Planeta(5e30, VERMELHO, (50e2, -25e2), 4e7, (COMPRIMENTO / 2 / escala, (ALTURA / 2 - 200) / escala))]
     while rodando:
         clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 rodando = False
             if event.type == pygame.KEYDOWN or event.type == pygame.KEYUP:  # Verifica se uma tecla foi pressionada ou soltada
-                escala = modificar_estado_teclas(teclas_seguradas, event, escala, deslocamento)
+                escala, pause, colisao = evento_teclas(teclas_seguradas, event, escala, deslocamento, pause, planetas, colisao)
         JANELA.fill(PRETO)
         mover_tela(deslocamento, teclas_seguradas)
-        if not colisao:
+        if (not colisao) and (not pause):
             for planeta in planetas:
                 forca = planeta.calcular_forcas(planetas)
                 planeta.atualizar_velocidade(forca)
         for planeta in planetas:
-            if not colisao:
+            if (not colisao) and (not pause):
                 planeta.atualizar_posicao()
                 if planeta.checar_colisoes(planetas):
                     colisao = True
